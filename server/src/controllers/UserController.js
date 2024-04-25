3dea7eedee2f78850927101efd389cc1a8413f54
Santijar05 <142135343+Santijar05@users.noreply.github.com>
2024-04-25 14:27:35 -0500

    feat: Created controllers methods

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -27,3 +27,30 @@ const createUser = async (req, res) => {
         res.status(500).json({error: "Something went wrong"});
     }
 };
+
+const getListUsers = async (req, res) => {
+    try{
+        console.log('Listar usuarios');
+        const users = await prisma.user.findMany();
+        res.status(200).json(users);
+    }catch(error){
+        console.log(error.message);
+        res.status(400).json({message: error.message});
+    }
+};
+
+const deleteUser = async (req, res) => {
+    try{ 
+        const {id} = req.body;
+        const user = await prisma.user.delete({
+            where: { id: id },
+        });
+        if(product){
+            res.status(200).json({message: "User deleted successfully"});
+        } else {
+            throw new Error("User not found");
+        }
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+}
