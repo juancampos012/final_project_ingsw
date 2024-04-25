71ea2b8e0f59d39ef774248c4d1b087d51e5eb32
juancampos012 <juancamposb012@gmail.com>
2024-04-25 16:42:39 -0500

    feat: create method updateUserByEmail, routes, index and connect to mongo

---
+++ server/src/routes/UserRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const multer = require('multer');
+const router = express.Router();
+const userController = require("../controllers/UserController")
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploads/users");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+})
+
+const upload = multer({storage});
+
+router.post('/new-user', upload.single("avatar"), userController.createUser);
+router.get("/list-user", userController.getListUsers); 
+router.get("/get-user-by-id", userController.getUserbyId); 
+router.get("/get-user-by-name", userController.getUserByName); 
+router.patch("/update-user", userController.updateUserByEmail); 
+router.delete("/delete-user", userController.deleteUser); 
+
+module.exports = router;
