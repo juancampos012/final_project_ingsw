383c6e2e37abcb76bee60923980229824db5f83e
Santijar05 <142135343+Santijar05@users.noreply.github.com>
2024-04-29 16:17:45 -0500

    feat: create the address controller

---
+++ server/src/controllers/AddressController.js
@@ -0,0 +1,75 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const createAddress = async (req, res) => {
+    try{
+        const { department, municipality, nomenclature, user, userId} = req.body;
+        const address = await prisma.address.create({
+            data: {
+                department,
+                municipality,
+                nomenclature,
+                user,
+                userId,
+            },
+        });
+        res.status(201).json({address});
+    }catch(error){
+        console.error(error);
+        res.status(500).json({error: "Something went wrong"});
+    }
+};
+
+const getListAddress = async (req, res) => {
+    try{
+        const address = await prisma.address.findMany();
+        res.status(200).json(address);
+    }catch(error){
+        console.log(error.message);
+        res.status(400).json({message: error.message});
+    }
+};
+
+const getAddressbyId = async (req, res) => {
+    try{
+        const {id} = req.query; 
+        const address = await prisma.address.findUnique({
+            where: { id: id },
+        });
+        res.status(200).json(address);
+    }catch(error){
+        res.status(400).json({message: error.message});
+    }
+}
+
+const deleteAddress = async (req, res) => {
+    try{ 
+        const {id} = req.body;
+        const address = await prisma.address.delete({
+            where: { id: id },
+        });
+        if(address){
+            res.status(200).json({message: "Address deleted successfully"});
+        } else {
+            throw new Error("Address not found");
+        }
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+}
+
+const updateAddressById = async (req, res) => {
+    try{
+        const {id} = req.body;
+        const { department, municipality, nomenclature, user, userId} = req.body;
+        const address = await prisma.address.update({
+            where: { id: id},
+            data: { department, municipality, nomenclature, user, userId},
+        });
+        res.status(201).json(address);
+    }catch(error){
+        res.status(400).json({message: error.message});
+    }
+}
+
+module.exports = {createAddress, getListAddress, getAddressbyId, deleteAddress, updateAddressById}
