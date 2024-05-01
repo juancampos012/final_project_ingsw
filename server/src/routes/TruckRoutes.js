0c56876b728363b0d72e39ffe7ee5aeb5a7308bf
juancampos012 <juancamposb012@gmail.com>
2024-04-30 22:53:30 -0500

    feat: created controller truck and routes truck

---
+++ server/src/routes/TruckRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const userController = require("../controllers/TruckController")
+
+router.post('/new-truck', userController.createTruck);
+router.get("/list-truck", userController.getListTruck); 
+router.get("/get-truck", userController.getTruckByLicencePlate); 
+router.patch("/update-truck", userController.updateTruckByLicencePlate); 
+router.delete("/delete-truck", userController.deleteTruckByLicencePlate); 
+
+module.exports = router;
