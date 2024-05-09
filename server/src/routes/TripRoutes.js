c8f2aa78daf110a9e0289b2566a49a77b857c45b
Santijar05 <142135343+Santijar05@users.noreply.github.com>
2024-05-09 17:11:18 -0500

    feat: Trip routes file

---
+++ server/src/routes/TripRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const userController = require("../controllers/TripController")
+
+router.post('/new-trip', userController.createTrip);
+router.get("/list-trips", userController.getListTrips); 
+router.get("/get-trip", userController.getTripbyId); 
+router.patch("/update-trip", userController.updateTripById); 
+router.delete("/delete-trip", userController.deleteTrip); 
+
+module.exports = router;
