3ec213413e8e1553f2f6ae675cc3b2e7a2af258a
Santijar05 <142135343+Santijar05@users.noreply.github.com>
2024-05-23 12:15:26 -0500

    feat: trip update route created

--- server/src/routes/TripRoutes.js
+++ server/src/routes/TripRoutes.js
@@ -5,6 +5,7 @@ const userController = require("../controllers/TripController")
 router.post('/new-trip', userController.createTrip);
 router.get("/list-trips", userController.getListTrips); 
 router.get("/get-trip", userController.getTripbyId); 
+router.patch("/update-trip", userController.updateTrip);
 router.delete("/delete-trip", userController.deleteTrip); 
 
 module.exports = router;
